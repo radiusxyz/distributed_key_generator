32c12801409dba4ba0e17e99ca24d3a647b77585
stompesi <stompesi@gmail.com>
2024-09-06 19:35:23 +0900

    Remove no used error

--- src/error.rs
+++ src/error.rs
@@ -1,46 +1,16 @@
 #[derive(Debug)]
 pub enum Error {
-    OpenConfig(std::io::Error),
-    ParseConfig(toml::de::Error),
     Database(radius_sequencer_sdk::kvstore::KvStoreError),
     RpcError(radius_sequencer_sdk::json_rpc::Error),
-    Deserialize(serde_json::Error),
-    InitializeLivenessClient(Box<dyn std::error::Error>),
-    Uninitialized,
-    EmptySequencerList,
-    LeaderIndexOutOfBound,
-    EmptyLeaderRpcUrl,
-    FetchResponse,
-    ClusterDown,
-    InvalidSequencerPort,
-    InvalidBlockHeight,
 
     LoadConfigOption,
     ParseTomlString,
-
     RemoveConfigDirectory,
     CreateConfigDirectory,
     CreateConfigFile,
     CreatePrivateKeyFile,
 
-    RegisterRpcUrl,
-    GetSequencerRpcUrlList,
-
-    GetSequencingInfo,
-    GetRollupMetadata,
-
-    PvdeZkpInvalid,
-    TryDecryptRawTransaction,
-
-    NotfoundRpcUrl,
-    NotFoundRollupState,
-    NotFoundCluster,
-    NotFoundClusterId,
-    NotFoundSequencingInfo,
-
-    InvalidTransactionOrder,
-
-    OtherSequencerRpcClientsCountNotCorrect,
+    NotFound,
 }
 
 unsafe impl Send for Error {}
